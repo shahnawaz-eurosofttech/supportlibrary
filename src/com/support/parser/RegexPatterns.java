1f3820f68c86618d9df4df5c6ea175d77b724fe6
shahnawaz-eurosofttech <shahnawaz@eurosofttech.co.uk>
2015-12-08 15:48:26 +0500

    Remove comments

--- src/com/support/parser/RegexPatterns.java
+++ src/com/support/parser/RegexPatterns.java
@@ -3,10 +3,6 @@ package com.support.parser;
 public class RegexPatterns {
 	public static final String REGEX_EMAIL = "^\\w+[\\w-\\.]*\\@\\w+((-\\w+)|(\\w*))\\.[a-z]{2,3}$";
 	public static final String REGEX_UK_NUMBERS = "^(\\+44\\s?7\\d{3}|\\(?07\\d{3}\\)?)\\s?\\d{3}\\s?\\d{3}$";
-	// public static final String REGEX_UK_POST_CODE = "^[A-Za-z]{1,2}[0-9A-Za-z]{1,2}[ ]?[0-9]{0,1}[A-Za-z]{2}$";
-	// public static final String REGEX_UK_POST_CODE_2 =
-	// "/((GIR 0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9][0-9]?)|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY])))) [0-9][ABD-HJLNP-UW-Z]{2}))/i";
-
 	public static final String REGEX_UK_POST_CODE_WS = "^(GIR 0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9][0-9]?)|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY])))) [0-9][A-BD-HJLNP-UW-Z]{2})";
 	public static final String REGEX_UK_POST_CODE_WOS = "^(GIR 0AA)|((([A-PR-UWYZ][0-9][0-9]?)|(([A-PR-UWYZ][A-HK-Y][0-9][0-9]?)|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))[0-9][A-BD-HJLNP-UW-Z]{2})";
 }
